73d5b2e0d7e81dcf452a15e122a7f73a377699f7
harbisn <harbisn5@gmail.com>
2022-04-17 17:31:33 +0700

    declare properties for employee

---
+++ src/app/employee.ts
@@ -0,0 +1,8 @@
+export class Employee {
+    declare id: number;
+    declare fullName: string;
+    declare address: string;
+    declare email: string;
+    declare phoneNumber: string;
+    declare role: string;
+}
