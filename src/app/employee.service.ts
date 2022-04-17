d807228af74500abb038c47f7dd5df25455370b9
harbisn <harbisn5@gmail.com>
2022-04-17 19:22:00 +0700

    menghubungkan angular dengan API serverside, tambah anotasi cross origin di serverside

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  private baseURL = "http://localhost:8089/api/employee"
+  constructor(private httpClient: HttpClient) { }
+
+  getEmployeeList(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(`${this.baseURL}`);
+  }
 }
